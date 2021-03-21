c1a70a3c818d7d57e6f05d6177b4167d1ec68926
Barender <gagan007bangs@gmail.com>
2021-03-21 19:51:21 +0530

    connected to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import products from "./data/products.js";
-const app = express();
+import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 8000;
+dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
@@ -16,6 +19,8 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
