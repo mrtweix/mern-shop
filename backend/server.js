cca7facdf417564d180ff1b084ea40b4df15792e
Barender <gagan007bangs@gmail.com>
2021-03-31 21:01:22 +0530

    added user authentication, middlewares & token generation

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from "colors";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
@@ -19,6 +20,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
