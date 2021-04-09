bf1ae396e583a7dd616f5ddc6f1cc841893d6aa3
Barender <gagan007bangs@gmail.com>
2021-04-09 19:16:30 +0530

    user edit screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/order/:id" component={OrderScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route exact path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
